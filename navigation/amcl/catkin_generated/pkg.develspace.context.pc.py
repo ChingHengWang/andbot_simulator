66f86c871214b144dbdec56349630b715796231b
ChingHengWang <qoogood1234@gmail.com>
2017-01-01 16:47:29 +0800

    add self navigation pkg

---
+++ navigation/amcl/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/pepper/catkin_ws/src/amcl/devel/include;/home/pepper/catkin_ws/src/amcl/include".split(';') if "/home/pepper/catkin_ws/src/amcl/devel/include;/home/pepper/catkin_ws/src/amcl/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "rosbag;roscpp;dynamic_reconfigure;tf".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lamcl_sensors;-lamcl_map;-lamcl_pf".split(';') if "-lamcl_sensors;-lamcl_map;-lamcl_pf" != "" else []
+PROJECT_NAME = "amcl"
+PROJECT_SPACE_DIR = "/home/pepper/catkin_ws/src/amcl/devel"
+PROJECT_VERSION = "1.14.0"
